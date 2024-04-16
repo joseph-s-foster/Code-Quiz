f781838ca5f81fe0adcdbb2e0bd943fba071a370
D3C0D1NG <joseph.s.foster@icloud.com>
2024-04-15 21:20:31 -0500

    styling and refactoring content

--- assets/score.js
+++ assets/score.js
@@ -1,11 +1,11 @@
-var highscores = JSON.parse(localStorage.getItem("scores")) || []
+const highscores = JSON.parse(localStorage.getItem("scores")) || []
 highscores.sort(function(a,b){
     return b.score-a.score;
 })
 
-for (var i = 0; i < highscores.length; i++){
-    var liEl = document.createElement("li")
-    liEl.textContent = highscores[i].name + ": " + highscores[i].score
+for (let i = 0; i < highscores.length; i++){
+    const liEl = document.createElement("li")
+    liEl.textContent = highscores[i].name + " : " + highscores[i].score
     document.querySelector("#scores").append(liEl)
 }
 
