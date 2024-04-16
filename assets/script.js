ad622ef679868ed0676577f2c7bc925c6ceadf2c
D3C0D1NG <joseph.s.foster@icloud.com>
2024-04-15 20:33:12 -0500

    styling and refactoring content

--- assets/script.js
+++ assets/script.js
@@ -1,159 +1,160 @@
-var time = 60;
-var currentQuestionIndex = 0;
-var timeEl = document.querySelector("#time");
-var startEl = document.querySelector(".start");
-var startButton = document.querySelector("#start-button");
-var questionsEl = document.querySelector("#questions");
-var choicesEl = document.querySelector("#choices");
-var quizEndEl = document.querySelector("#quiz-end");
-var finalScore = document.querySelector("#final-score");
-var nameEl = document.querySelector("#name");
-var saveScoreButton = document.querySelector("#save-score");
-var leaderboardEl = document.querySelector("#leaderboard");
-var scoresEl = document.querySelector("#scores");
-var timeInterval;
-var questions = [
-    {
-        title: 'Arrays are enclosed within ____.',
-        choices: ['parenthesis', 'quotations', 'hashtags', 'brackets'],
-        answer: 'brackets',
-    },
-    {
-        title: 'Common data types DO NOT include:',
-        choices: ['strings', 'booleans', 'alerts', 'numbers'],
-        answer: 'alerts',
-    },
-    {
-        title: 'Which operator is used for strict equality ____.',
-        choices: ['=', '==', '===', '!=='],
-        answer: '===',
-    },
-    {
-        title: 'Arrays in JavaScript can be used to store ____.',
-        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-        answer: 'all of the above',
-    },
-    {
-        title: 'What does "NaN" represent in JavaScript?',
-        choices: ['not a number', 'null', 'no argument', 'no activation'],
-        answer: 'not a number',
-    },
-    {
-        title: 'Which function is used to parse a JSON string?',
-        choices: ['parseJSON()', 'toJSON()', 'JSON.parse()', 'stringify()'],
-        answer: 'JSON.parse()',
-    },
-    {
-        title: 'How do you comment lines in JavaScript?',
-        choices: ['##', '//', '--', '||'],
-        answer: '//',
-    },
-    {
-        title: 'Which function converts a string to lowercase?',
-        choices: ['toLower()', 'lowerCase()', 'toLowerCase()', 'stringToLower()'],
-        answer: 'toLowerCase()',
-    },
-    {
-        title: 'How do you declare a constant in JavaScript?',
-        choices: ['const', 'for', 'let', 'var'],
-        answer: 'const',
-    },
-    {
-        title: 'Which operator combines two or more strings?',
-        choices: ['+=', '++', '&', '+'],
-        answer: '+',
-    },
+let time = 60;
+let currentQuestionIndex = 0;
+const timeEl = document.querySelector("#time");
+const startEl = document.querySelector(".start");
+const startButton = document.querySelector("#start-button");
+const questionsEl = document.querySelector("#questions");
+const choicesEl = document.querySelector("#choices");
+const quizEndEl = document.querySelector("#quiz-end");
+const finalScore = document.querySelector("#final-score");
+const nameEl = document.querySelector("#name");
+const saveScoreButton = document.querySelector("#save-score");
+const leaderboardEl = document.querySelector("#leaderboard");
+const scoresEl = document.querySelector("#scores");
+let timeInterval;
+
+const questions = [
+  {
+    title: "Arrays are enclosed within ____.",
+    choices: ["parenthesis", "quotations", "hashtags", "brackets"],
+    answer: "brackets",
+  },
+  {
+    title: "Data types DO NOT include ____.",
+    choices: ["strings", "booleans", "variables", "numbers"],
+    answer: "variables",
+  },
+  {
+    title: "____ represents strict equality.",
+    choices: ["=", "==", "===", "!=="],
+    answer: "===",
+  },
+  {
+    title: "Arrays can be used to store ____.",
+    choices: ["strings", "arrays", "booleans", "all of the above"],
+    answer: "all of the above",
+  },
+  {
+    title: '"NaN" represents ____.',
+    choices: ["not a number", "null", "no argument", "no activation"],
+    answer: "not a number",
+  },
+  {
+    title: "____ parses a JSON string.",
+    choices: ["parseJSON()", "toJSON()", "JSON.parse()", "stringify()"],
+    answer: "JSON.parse()",
+  },
+  {
+    title: "Comment out lines using ____.",
+    choices: ["##", "//", "--", "||"],
+    answer: "//",
+  },
+  {
+    title: "____ converts a string to lower case.",
+    choices: ["toLower()", "lowerCase()", "toLowerCase()", "stringToLower()"],
+    answer: "toLowerCase()",
+  },
+  {
+    title: "____ declares a constant.",
+    choices: ["const", "for", "let", "var"],
+    answer: "const",
+  },
+  {
+    title: "____ combines datatypes.",
+    choices: ["+=", "++", "&", "+"],
+    answer: "+",
+  },
 ];
 
 function startQuiz() {
-    startEl.classList.add("hide");
-    timeInterval = setInterval(countdown, 1000);
-    timeEl.textContent = time;
-    questionsEl.removeAttribute("class");
-    displayQuestion()
+  startEl.classList.add("hide");
+  timeInterval = setInterval(countdown, 1000);
+  timeEl.textContent = time;
+  questionsEl.removeAttribute("class");
+  displayQuestion();
+
+  nameEl.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      saveScore();
+    }
+  });
 }
 
 function displayQuestion() {
-    var currentQuestion = questions[currentQuestionIndex];
-    var titleEl = document.querySelector("#questions-title");
-    titleEl.textContent = currentQuestion.title;
-    choicesEl.innerHTML = "";
+  const currentQuestion = questions[currentQuestionIndex];
+  const titleEl = document.querySelector("#questions-title");
+  titleEl.textContent = currentQuestion.title;
+  titleEl.classList.add("questions");
+  choicesEl.innerHTML = "";
 
-    for (var i = 0; i < currentQuestion.choices.length; i++) {
-        var choice = currentQuestion.choices[i];
-        var choiceNode = document.createElement("button");
-        choiceNode.setAttribute("class", "choice");
-        choiceNode.setAttribute("value", choice);
-        // var choiceLetter = String.fromCharCode(65 + i);
-        // choiceNode.textContent = choiceLetter + '. ' + choice;
-        choiceNode.textContent = choice;
-        choicesEl.appendChild(choiceNode);
-    }
-    
+  for (let i = 0; i < currentQuestion.choices.length; i++) {
+    const choice = currentQuestion.choices[i];
+    const choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+    choiceNode.textContent = choice;
+    choicesEl.appendChild(choiceNode);
+  }
 }
 
 function questionClick(event) {
-    var buttonEl = event.target;
+  const buttonEl = event.target;
 
-    if (!buttonEl.matches(".choice")) {
-        return;
+  if (!buttonEl.matches(".choice")) {
+    return;
+  }
+
+  if (buttonEl.value !== questions[currentQuestionIndex].answer) {
+    time -= 5;
+
+    if (time < 0) {
+      time = 0;
     }
+  }
 
-    if (buttonEl.value !== questions[currentQuestionIndex].answer) {
-        time -= 5;
+  timeEl.textContent = time;
 
-        if (time < 0) {
-            time = 0;
-        }
-    }
+  currentQuestionIndex++;
 
-    timeEl.textContent = time;
-
-    currentQuestionIndex++;
-
-    if (time <= 0 || currentQuestionIndex === questions.length) {
-        quizEnd();
-    } else {
-        displayQuestion();
-    }
+  if (time <= 0 || currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    displayQuestion();
+  }
 }
 
 function countdown() {
-    time--;
-    timeEl.textContent = time;
-    if (time <= 0) {
-        quizEnd()
-    }
+  time--;
+  timeEl.textContent = time;
+  if (time <= 0) {
+    quizEnd();
+  }
 }
 
 function quizEnd() {
-    clearInterval(timeInterval);
+  clearInterval(timeInterval);
 
-    var quizEndEl = document.querySelector('#quiz-end');
-    quizEndEl.classList.remove("hide");
+  quizEndEl.classList.remove("hide");
 
-    var finalScoreEl = document.querySelector('#final-score');
-    finalScoreEl.textContent = time;
+  finalScore.textContent = time;
 
-    questionsEl.setAttribute("class", "hide");
+  questionsEl.setAttribute("class", "hide");
 
-    saveScoreButton.onclick = saveScore
+  saveScoreButton.onclick = saveScore;
 }
 
 function saveScore() {
-    var highscores = JSON.parse(localStorage.getItem("scores")) || []
-    var score = {
-        name: nameEl.value,
-        score: time
-    }
+  const highscores = JSON.parse(localStorage.getItem("scores")) || [];
+  const score = {
+    name: nameEl.value,
+    score: time,
+  };
 
-    highscores.push(score)
-    localStorage.setItem("scores", JSON.stringify(highscores))
+  highscores.push(score);
+  localStorage.setItem("scores", JSON.stringify(highscores));
 
-    window.location.href = "scores.html"
-
+  window.location.href = "scores.html";
 }
 
 startButton.addEventListener("click", startQuiz);
-
 choicesEl.addEventListener("click", questionClick);
